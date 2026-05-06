fafc10d89eec2bc89546c47c1b6c0f3dce950c50
davimoreiradev <davimoreira2203@gmail.com>
2026-05-05 23:40:21 -0300

    Modelagem do banco de dados, NinjaModel e MissoesModel concluidos, Anotations feitas para relacionamento de tabelas

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -1,7 +1,10 @@
 package dev.java10x.CadastroDeNinjas.Ninjas;
 
 
+import dev.java10x.CadastroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
+
+import java.util.List;
 
 @Entity
 @Table(name = "tb_cadastro")
@@ -13,6 +16,10 @@ public class NinjaModel {
     private String nome;
     private String email;
     private int idade;
+    @ManyToOne
+    @JoinColumn(name = "missoes_id")
+    private MissoesModel missoes;
+
 
     public NinjaModel(String nome, String email, int idade) {
         this.nome = nome;
