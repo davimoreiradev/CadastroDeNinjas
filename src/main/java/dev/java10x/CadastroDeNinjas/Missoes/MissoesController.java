fabd5df89d3918715dddb97b8a2cf326e2e081bf
davimoreiradev <davimoreira2203@gmail.com>
2026-05-20 20:40:04 -0300

    feat: cria as rotas do controller com o mock

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesController.java
@@ -1,4 +1,29 @@
 package dev.java10x.CadastroDeNinjas.Missoes;
 
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("missoes")
 public class MissoesController {
+
+    @GetMapping("/listar")
+    public String listarMissao() {
+        return "Missoes listada";
+    }
+
+    @PostMapping("/criar")
+    public String criarMissao() {
+        return "Missao criada";
+    }
+
+    @PutMapping("/alterar")
+    public String alterarMissao() {
+        return "Missao alterada";
+    }
+
+    @DeleteMapping("/deletar")
+    public String deletarMissao() {
+        return "Missao deletada";
+    }
 }
